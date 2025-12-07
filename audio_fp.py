fa8de686b335073239bc1e907c78ed30293f2608
sevencoded <vupav40@gmail.com>
2025-12-07 01:33:52 +0100

    Update audio_fp.py

--- audio_fp.py
+++ audio_fp.py
@@ -1,6 +1,6 @@
 import numpy as np
 import librosa
-from .utils import sha256_bytes
+from utils import sha256_bytes
 
 def extract_audio_fingerprint(audio_data, sample_rate):
     mfcc = librosa.feature.mfcc(y=audio_data, sr=sample_rate, n_mfcc=20)
