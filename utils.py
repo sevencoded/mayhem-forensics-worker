34c81117c3f04da99cbc27b82acf9a5403d3b208
sevencoded <vupav40@gmail.com>
2025-12-10 00:27:55 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,8 +7,9 @@ SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def upload_file(path: str, data: bytes, mime: str):
+    # PRAVILAN SUPABASE UPLOAD FORMAT
     supabase.storage.from_("main_videos").upload(
-        file=data,
-        path=path,
-        file_options={"content-type": mime}
+        path,
+        data,
+        {"content-type": mime}
     )
