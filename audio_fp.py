d6d0e3c5348e1e055d01be91c4e65d71b547f356
sevencoded <vupav40@gmail.com>
2025-12-09 23:27:25 +0100

    Update audio_fp.py

--- audio_fp.py
+++ audio_fp.py
@@ -1,9 +1,4 @@
-import librosa
-import numpy as np
-import hashlib
+import uuid
 
-def generate_audio_fingerprint(filepath):
-    y, sr = librosa.load(filepath, sr=None)
-    onset = librosa.onset.onset_strength(y=y, sr=sr)
-    fp = hashlib.sha256(onset.tobytes()).hexdigest()
-    return fp
+def extract_audio_fingerprint(path):
+    return uuid.uuid4().hex
