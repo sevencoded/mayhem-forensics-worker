6ae750aebac5fb45ecfa0a5b724a8d9c02bf5d7b
sevencoded <vupav40@gmail.com>
2025-12-09 23:47:40 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,14 @@
 import os
-from supabase import create_client
+from supabase import create_client, Client
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-def upload_file(path, bytes, mime):
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def upload_file(path: str, data: bytes, mime: str):
     supabase.storage.from_("main_videos").upload(
-        path, bytes, {"content-type": mime}
+        file=data,
+        path=path,
+        file_options={"content-type": mime}
     )
