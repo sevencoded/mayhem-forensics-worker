224c8ca41065a1e984d0abce66b74299a613932a
sevencoded <vupav40@gmail.com>
2025-12-09 21:07:14 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,21 +1,11 @@
 import os
-import tempfile
 from supabase import create_client
 
-supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_SERVICE_KEY"))
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-def download_file(path):
-    res = supabase.storage.from_("main_videos").download(path)
-    temp = tempfile.NamedTemporaryFile(delete=False, suffix=".video")
-    temp.write(res)
-    temp.close()
-    return temp.name
-
-def upload_file(path, bytes_data, content_type):
-    supabase.storage.from_("main_videos").upload(path, bytes_data, {
-        "contentType": content_type,
-        "upsert": "true"
-    })
-
-def delete_file(path):
-    supabase.storage.from_("main_videos").remove([path])
+def upload_file(path, bytes, mime):
+    supabase.storage.from_("main_videos").upload(
+        path, bytes, {"content-type": mime}
+    )
